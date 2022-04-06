1616e3a89bd85b34c1239917530ab2d965c69ef2
Mishoo4523 <102149794+Mishoo4523@users.noreply.github.com>
2022-04-06 20:28:22 +0300

    Add files via upload

---
+++ 2.1-functions.js
@@ -0,0 +1,11 @@
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} people and its capital city is ${capitalCity}`;
+}
+
+let a = describeCountry('finland', ' 6 milion', 'Helsinki);
+let b = describeCountry('Italy', ' 80 milion', 'Rome');
+let c = describeCountry('France', ' 80 milion', 'Paris');
+
+console.log(a);
+console.log(b);
+taly
