54bbe96d580b5d14798fe3bf9f3f00742237a482
Mishoo4523 <102149794+Mishoo4523@users.noreply.github.com>
2022-04-07 18:23:42 +0300

    Update 2.2-fortune_teller..js

--- 2.2-fortune_teller..js
+++ 2.2-fortune_teller..js
@@ -5,5 +5,5 @@ function tellFortune(jobTitle, location, partnerName, numberOfChildren ){
     return `You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children.`
 }
 
-let  myLife = tellFortune('programmer', 'Tel-Aviv', 'Sharon', 2 );
-console.log(myLife)
+let  info = tellFortune('programmer', 'Tel-Aviv', 'Sharon', 2 );
+console.log(info)
