2c6f5dfd8181c5c65e034851924458c10de2aeba
Mishoo4523 <102149794+Mishoo4523@users.noreply.github.com>
2022-04-07 18:22:15 +0300

    Add files via upload

---
+++ 2.2-fortune_teller..js
@@ -0,0 +1,9 @@
+
+
+
+function tellFortune(jobTitle, location, partnerName, numberOfChildren ){
+    return `You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children.`
+}
+
+let  myLife = tellFortune('programmer', 'Tel-Aviv', 'Sharon', 2 );
+console.log(myLife)
