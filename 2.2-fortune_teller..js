83f73cb2590e1ad0553742d6bddcaaa5996fa427
Mishoo4523 <102149794+Mishoo4523@users.noreply.github.com>
2022-04-10 17:44:40 +0300

    Add files via upload

--- 2.2-fortune_teller..js
+++ 2.2-fortune_teller..js
@@ -7,3 +7,17 @@ function tellFortune(jobTitle, location, partnerName, numberOfChildren ){
 
 let  info = tellFortune('programmer', 'Tel-Aviv', 'Sharon', 2 );
 console.log(info)
+
+
+let getFullDate = () =>{
+    let d = new Date();
+    const [day, year] = [ d.getDate(), d.getFullYear()];
+    const dayName = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"][new Date().getDay()]
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"][new Date().getMonth()]  ;
+    
+    return `today is a ${dayName} the ${day} of ${monthNames} ,${year} `;
+}
+let date = getFullDate();
+console.log(date);
+
