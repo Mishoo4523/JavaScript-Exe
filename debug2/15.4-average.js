636578fd41f198e653cfbd795a58a38967af8ff0
Mishoo4523 <102149794+Mishoo4523@users.noreply.github.com>
2022-04-19 08:33:30 +0300

    Add files via upload

---
+++ debug2/15.4-average.js
@@ -0,0 +1,17 @@
+
+  
+function calcAverage(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+
+console.log(calcAverage([85, 90, 92]));
+
+// line 3 arr.length
+//  used console.log
+// return was NaN
+// sum needed to be drfined as 0.
